df2a93216efa6e2ace42b89484a92fa67eacf354
Ogawakin <htriolet@ymail.com>
2022-01-12 23:37:24 +0100

    Update tp_3.py

--- tp_3.py
+++ tp_3.py
@@ -3,11 +3,9 @@ import matplotlib.pyplot as plt
 import math as m
 import scipy.stats as stats
 
-## Problème 1
-
-poids_kg = [0.499, 0.509, 0.501, 0.494, 0.498, 0.497, 0.504, 0.506, 0.502, 0.496, 0.495, 0.493, 0.507, 0.505, 0.503, 0.491]
-poids_g = [85.06, 91.44, 87.93, 89.02, 87.28, 82.34, 86.23, 84.16, 88.56, 90.45, 84.91, 89.90, 85.52, 86.75, 88.54, 87.90]
-
+# Dans ce Tp, on cherche à estimer, via
+# intervalle de confiance, l'espérance
+# d'un échantillon statistique
 
 def moyenne_empirique(liste):
     """ fonction qui calcule de manière empirique
@@ -38,8 +36,10 @@ plt.show()
 # ce qui donnera sqrt(n)*(moyenne_empirique - moyenne)/ecart_type_empirique
 
 def intervalle_confiance(alpha, liste):
-    """ calcul d'un intervalle de confiance de 95%
-        ou 99% (aplha = 0.01 ou 0.05) """
+    """ calcul d'un intervalle de confiance
+        dans le cadre d'une variance inconnue
+        et donc d'une recherche du fractile
+        selon la loi de student (St(n-1))"""
 
     moyenne = moyenne_empirique(liste)
     ecart_type = m.sqrt(variance_empirique(liste))
@@ -52,21 +52,30 @@ def intervalle_confiance(alpha, liste):
     intervalle_droit = moyenne + t*ecart_type/(m.sqrt(len(liste)))
     return [round(intervalle_gauche, 3), round(intervalle_droit, 3)]
 
+## Problème 1
+
+poids_kg = [0.499, 0.509, 0.501, 0.494, 0.498, 0.497, 0.504, 0.506, 0.502, 0.496, 0.495, 0.493, 0.507, 0.505, 0.503, 0.491]
+poids_g = [85.06, 91.44, 87.93, 89.02, 87.28, 82.34, 86.23, 84.16, 88.56, 90.45, 84.91, 89.90, 85.52, 86.75, 88.54, 87.90]
+
 interval1 = intervalle_confiance(0.05, poids_kg)
 interval2 = intervalle_confiance(0.05, poids_g)
 interval3 = intervalle_confiance(0.01, poids_kg)
 interval4 = intervalle_confiance(0.01, poids_g)
 
 print("Pour pots confiture (en kg), intervalle à 95% : ", interval1)
-print("Pour avocats (en g), intervalle à 95% : ", interval2)
 print("Pour pots confiture (en kg), intervalle à 99% : ", interval3)
+print("Pour avocats (en g), intervalle à 95% : ", interval2)
 print("Pour avocats (en g), intervalle à 99% : ", interval4)
 
 ## Problème 2
 
 # selon l'étude, 95/500 sont satisfait par la compagnie
 # on peut donc en approximer une moyenne de 95/500
+# Donc la fonction moyenne_empirique doit trouver 95/500
 
+population = [1 for k in range(95)] + [0 for k in range(405)]
+intervalle = intervalle_confiance(0.01, population)
+print("\nLa proportion de voyageurs satisfaits se situe entre ", intervalle)
 
 ## Problème 3
 
@@ -76,15 +85,15 @@ print("Pour avocats (en g), intervalle à 99% : ", interval4)
 # de valeurs de n exdpériences de Bernoulli.
 # bernoulli.rvs(p, n)
 
-print("Taille de l'échantillon voulue : ", end="")
+print("\nTaille de l'échantillon voulue : ", end="")
 n = int(input())
 
 # ici, p = 1/2 = 0.5
 echantillon = stats.bernoulli.rvs(0.5, size=n)
 echantillon = echantillon.tolist()
 
-print(f"Pour un échantillon de {n} expériences de Bernoulli ", end="")
-print("indépendantes, voici l'intervalle de confiance à ", end="")
+print(f"Pour un échantillon de {n} expériences de Bernoulli, ", end="")
+print("intervalle de confiance à ", end="")
 print("95% pour le paramètre p : ", intervalle_confiance(0.05, echantillon))
 
 # plus le nombre d'expérience augmente, plus les bornes se resserent autour
